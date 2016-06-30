d7460fc632d0f34fae50009a9fa790d4dfeb37fe
Shannon Turner <shannon.v.turner@gmail.com>
2016-06-29 20:37:54 -0400

    Fixed some bugs, made it easier to debug

--- txt2pi/views.py
+++ txt2pi/views.py
@@ -41,18 +41,18 @@ class BudgieCamView(TemplateView):
         if text_message:
             if BUDGIE_PASSPHRASE in text_message:
                 try:
-                    budgie_filename = '{1}.png'.format(''.join([str(x) for x in time.localtime()[:6]]))
-                    subprocess.call(['raspistill', '-o',  "{0}{1}".format(BUDGIE_FILE_PATH, budgie_filename)])
-                except Exception:
-                    print "[ERROR] Call to raspistill failed; could not take photo"
+                    budgie_filename = '{0}.png'.format(''.join([str(x) for x in time.localtime()[:6]]))
+                    subprocess.call(['raspistill', '-t 500 -o',  "{0}{1}".format(BUDGIE_FILE_PATH, budgie_filename)])
+                except Exception, e:
+                    print "[ERROR] Call to raspistill failed; could not take photo ({0}: {1}{2})".format(e, BUDGIE_FILE_PATH, budgie_filename)
                     context['response'] = '500'
                 else:
                     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
                     client.messages.create(
-                        to=budgiecam_phone_number, 
-                        from_=requesting_phone_number, 
-                        body="{0}".format(budgie_filename), 
-                        media_url="{0}{1}{2}".format(RASPI_IP, BUDGIE_WEB_PATH, budgie_filename), 
+                        to=requesting_phone_number,
+                        from_=budgiecam_phone_number,
+                        body="{0}".format(budgie_filename),
+                        media_url="{0}{1}{2}".format(RASPI_IP, BUDGIE_WEB_PATH, budgie_filename),
                     )
                     context['response'] = '200'
             else:
