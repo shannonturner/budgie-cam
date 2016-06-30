f84a174ae71986bb1ca058695c594afb9a6ae621
Shannon Turner <shannon.v.turner@gmail.com>
2016-06-29 21:10:32 -0400

    jpg not png

--- txt2pi/views.py
+++ txt2pi/views.py
@@ -41,7 +41,7 @@ class BudgieCamView(TemplateView):
         if text_message:
             if BUDGIE_PASSPHRASE in text_message:
                 try:
-                    budgie_filename = '{0}.png'.format(''.join([str(x) for x in time.localtime()[:6]]))
+                    budgie_filename = '{0}.jpg'.format(''.join([str(x) for x in time.localtime()[:6]]))
                     subprocess.call(['raspistill', '-t', '500', '-o', "{0}{1}".format(BUDGIE_FILE_PATH, budgie_filename)])
                 except Exception, e:
                     print "[ERROR] Call to raspistill failed; could not take photo ({0}: {1}{2})".format(e, BUDGIE_FILE_PATH, budgie_filename)
