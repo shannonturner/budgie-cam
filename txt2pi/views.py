b86267af9977cc6ecd266e17231792e563fbe3c1
Shannon Turner <shannon.v.turner@gmail.com>
2016-07-02 10:47:12 -0400

    Added video capability

--- txt2pi/views.py
+++ txt2pi/views.py
@@ -39,22 +39,38 @@ class BudgieCamView(TemplateView):
         context = {}
 
         if text_message:
-            if BUDGIE_PASSPHRASE in text_message:
-                try:
-                    budgie_filename = '{0}.jpg'.format(''.join([str(x) for x in time.localtime()[:6]]))
-                    subprocess.call(['raspistill', '-t', '5000', '-o', "{0}{1}".format(BUDGIE_FILE_PATH, budgie_filename)])
-                except Exception, e:
-                    print "[ERROR] Call to raspistill failed; could not take photo ({0}: {1}{2})".format(e, BUDGIE_FILE_PATH, budgie_filename)
-                    context['response'] = '500'
+            if BUDGIE_PASSPHRASE in text_message.lower():
+                if 'video' in text_message.lower():
+                    try:
+                        budgie_filename = '{0}.h264'.format(''.join([str(x) for x in time.localtime()[:6]]))
+                        # raspivid -o video.h264 -t 10000
+                        subprocess.call(['raspivid', '-t', '30000','-o', '{0}{1}'.format(BUDGIE_FILE_PATH, budgie_filename)])
+                    except Exception, e:
+                        print "[ERROR] Call to raspivid failed; could not take video ({0}: {1}{2})".format(e, BUDGIE_FILE_PATH, budgie_filename)
+                    else:
+                        client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+                        client.messages.create(
+                            to=requesting_phone_number,
+                            from_=budgiecam_phone_number,
+                            body="Video ready here: {0}{1}{2}".format(RASPI_IP, BUDGIE_WEB_PATH, budgie_filename)
+                        )
+                        context['response'] = '200'
                 else:
-                    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-                    client.messages.create(
-                        to=requesting_phone_number,
-                        from_=budgiecam_phone_number,
-                        body="{0}".format(budgie_filename),
-                        media_url="{0}{1}{2}".format(RASPI_IP, BUDGIE_WEB_PATH, budgie_filename),
-                    )
-                    context['response'] = '200'
+                    try:
+                        budgie_filename = '{0}.jpg'.format(''.join([str(x) for x in time.localtime()[:6]]))
+                        subprocess.call(['raspistill', '-t', '5000', '-o', "{0}{1}".format(BUDGIE_FILE_PATH, budgie_filename)])
+                    except Exception, e:
+                        print "[ERROR] Call to raspistill failed; could not take photo ({0}: {1}{2})".format(e, BUDGIE_FILE_PATH, budgie_filename)
+                        context['response'] = '500'
+                    else:
+                        client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+                        client.messages.create(
+                            to=requesting_phone_number,
+                            from_=budgiecam_phone_number,
+                            body="{0}".format(budgie_filename),
+                            media_url="{0}{1}{2}".format(RASPI_IP, BUDGIE_WEB_PATH, budgie_filename),
+                        )
+                        context['response'] = '200'
             else:
                 context['response'] = '401'
         else:
